968f7c2c6510e00adaa5049f9d5245e1cf4b13e1
adirn26 <adirn26@gmail.com>
2023-05-01 19:29:15 +0530

    Integrating-backend-routes

--- Client/src/components/Navbar.js
+++ Client/src/components/Navbar.js
@@ -1,87 +1,193 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Fragment, useRef, useState } from 'react'
+import { Dialog, Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import {
+  CurrencyRupeeIcon
+} from '@heroicons/react/20/solid'
+import {useDispatch} from 'react-redux'
+import { addNewExpense } from '../redux/actions'
 
-const navigation = [
-  { name: 'Home', href: '#', current: true },
-  { name: 'Add Expense', href: '#', current: false },
-]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false)
+  const cancelButtonRef = useRef(null)
+  const [expenseAmount, setExpenseAmount] = useState(0)
+  const [expenseName, setExpenseName] = useState("")
+  const dispatch = useDispatch();
+
+  const onAddExpense = (e) => {
+    setOpen(false)
+    const newExpense = {
+      "name" : expenseName,
+      "amount" : expenseAmount
+    }
+    dispatch(addNewExpense(newExpense)) 
+    console.log(newExpense)
+  }
+
+  const navigation = [
+    { name: 'Home', href: '#', current: true, onClick: () => { console.log("Home") } },
+    { name: 'Add Expense', href: '#', current: false, onClick: () => { setOpen(true) } },
+  ]
+
   return (
-    <Disclosure as="nav" className="bg-gray-800">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex-shrink-0">
-                <h1 className="text-3xl font-medium text-white">Pocket</h1>
-              </div>
-              <div className="flex-shrink-0">
-                <h1 className="text-3xl font-medium text-blue-500">Guard</h1>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
+    <>
+      <Disclosure as="nav" className="bg-gray-800">
+        {({ open }) => (
+          <>
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+              <div className="relative flex h-16 items-center justify-between">
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                  {/* Mobile menu button*/}
+                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
+                <div className="flex-shrink-0">
+                  <h1 className="text-3xl font-medium text-white">Pocket</h1>
+                </div>
+                <div className="flex-shrink-0">
+                  <h1 className="text-3xl font-medium text-blue-500">Guard</h1>
+                </div>
+                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                  <div className="hidden sm:ml-6 sm:block">
+                    <div className="flex space-x-4">
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                            'rounded-md px-3 py-2 text-sm font-medium'
+                          )}
+                          aria-current={item.current ? 'page' : undefined}
+                          onClick={item.onClick}
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
                   </div>
                 </div>
+                <div>
+                  <button className="bg-500 hover:bg-blue-500 text-white rounded-md px-3 py-2 text-sm font-medium">
+                    Sign In
+                  </button>
+                </div>
               </div>
-              <div>
-                <button className="bg-500 hover:bg-blue-500 text-white rounded-md px-3 py-2 text-sm font-medium">
-                  Sign In
-                </button>
+            </div>
+
+            <Disclosure.Panel className="sm:hidden">
+              <div className="space-y-1 px-2 pb-3 pt-2">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'block rounded-md px-3 py-2 text-base font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
               </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                        <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                          Add Expense
+                        </Dialog.Title>
+                        <div className="relative mt-2 rounded-md shadow-sm">
+                          <input
+                            type="text"
+                            name="price"
+                            id="price"
+                            className="block w-full rounded-md py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="Enter Name"
+                            onChange={(e) => setExpenseName(e.target.value)}
+                          />
+                        </div>
+                        <div className="relative mt-2 rounded-md shadow-sm">
+                          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                            <span className="text-gray-500 sm:text-sm">₹</span>
+                          </div>
+                          <input
+                            type="text"
+                            name="price"
+                            id="price"
+                            className="block w-full rounded-md py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="Enter Amount"
+                            onChange={(e) => setExpenseAmount(e.target.value)}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
+                      onClick={() => onAddExpense()}
+                    >
+                      Save
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      onClick={() => setOpen(false)}
+                      ref={cancelButtonRef}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
             </div>
           </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+        </Dialog>
+      </Transition.Root>
+    </>
   )
 }
--- Client/src/redux/actions/index.js
+++ Client/src/redux/actions/index.js
@@ -1,12 +1,30 @@
 import axios from "axios";
-import { ADDNEW_EXPENSE } from "../constants/action-types";
+import { ADDNEW_EXPENSE, DELETE_EXPENSE, GETALL_EXPENSE } from "./type";
 
 const API_URL = "http://localhost:8000";
 
 export const addNewExpense = (data) => async (dispatch) => {
     try {
-        const res = await axios.post(`${API_URL}/expenses`, data);
+        const res = await axios.post(`${API_URL}/addExpense`, data);
         dispatch({ type: ADDNEW_EXPENSE, payload: res.data });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getAllExpenses = () => async (dispatch) => {
+    try {
+        const res = await axios.get(`${API_URL}/getExpenses`);
+        dispatch({ type: GETALL_EXPENSE, payload: res.data });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deleteExpense = (id) => async (dispatch) => {
+    try {
+        const res = await axios.delete(`${API_URL}/deleteExpense/${id}`);
+        dispatch({ type: DELETE_EXPENSE, payload: res.data });
     } catch (error) {
         console.log(error);
     }
